9a1b36a9257c9c7742e2ac6a15f262afba5cd5c2
Tai <4382091+taisukemino@users.noreply.github.com>
2021-05-26 10:40:35 +0900

    feat: rewrite the deploy script for the upgradable contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,14 @@
-const { BigNumber } = require("@ethersproject/bignumber");
-
-//), We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
-// const {get30UniswapProce } = require('./utils/uniswapQuery.js')
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
   let WETHPriceInDAI30Days = [
     3000, 2900, 3000, 2900, 3000, 2900, 3000, 2900, 3000, 2900, 3000, 2900,
     3000, 2900, 3000, 2900, 3000, 2900, 3000, 2900, 3000, 2900, 3000, 2900,
     3000, 2900, 3000, 2900, 3000, 2900
   ];
 
-  // We get the contract to deploy
-  const VolToken = await hre.ethers.getContractFactory("VolToken");
-  // const VolToken = await hre.l2ethers.getContractFactory("VolToken");
-  const volToken = await VolToken.deploy(
+  const VolToken = await hre.l2ethers.getContractFactory("VolToken");
+  console.log("Deploying VolToken...");
+  const volToken = await upgrades.deployProxy(VolToken, [
     "ETH 30 day Vol",
     "ETH30VOL",
     WETHPriceInDAI30Days,
@@ -33,13 +17,11 @@ async function main() {
     "0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11",
     false,
     50
-  );
+  ]);
   await volToken.deployed();
   console.log("VolToken deployed to:", volToken.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
