78db2201b2fe2c2d8c310831493248e0662b8738
Tai <4382091+taisukemino@users.noreply.github.com>
2021-05-27 14:02:54 +0900

    build: update the solidity version

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -1,1 +1,1 @@
-pragma solidity 0.7.6;
+pragma solidity 0.8.0;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,7 +50,7 @@ module.exports = {
     }
   },
   solidity: {
-    version: "0.7.6",
+    version: "0.8.0",
     settings: {
       optimizer: {
         enabled: true,
